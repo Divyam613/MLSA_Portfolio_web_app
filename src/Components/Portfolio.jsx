7a6d6d6662cb0d030992521c4182493c6c3a52d4
Divyam613 <119338081+Divyam613@users.noreply.github.com>
2023-12-25 13:48:47 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -60,7 +60,7 @@ const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+      <div className="card_container">
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
